4d2a1a31620e70c8f9d3c649d3cc70e0803936ac
HAK1580 <aliofficial1580630@gmail.com>
2026-09-05 00:22:39 +0500

    fixed cors issue

--- server.js
+++ server.js
@@ -9,13 +9,12 @@ require('dotenv').config();
 app.use(express.json());
 
 const corsOptions = {
-  origin: 'https://expense-tracker-1580.netlify.app',
+  origin: ['https://expense-tracker-1580.netlify.app', 'http://localhost:5173'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
 };
 app.use(cors(corsOptions));
-app.use(cors());//for local developement
 
 async function main(){
     await mongoose.connect(process.env.MONGO_URI);
