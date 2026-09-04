4696a704d9cabe3c1751e46004ed3ead38fe0b29
HAK1580 <aliofficial1580630@gmail.com>
2026-09-04 22:50:24 +0500

    look at the cors

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ require('dotenv').config();
 app.use(express.json());
 
 const corsOptions = {
-  origin: ['https://expense-tracker-1580.netlify.app', 'http://localhost:5173'],
+  origin: ['https://expense-tracker-1580.netlify.app','http://localhost:5173'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
