bf34d37d64f6debf84fcd727bb60196dd5eaa13b
HAK1580 <aliofficial1580630@gmail.com>
2026-09-04 22:55:45 +0500

    finally  fixed the cors error

--- server.js
+++ server.js
@@ -9,13 +9,13 @@ require('dotenv').config();
 app.use(express.json());
 
 const corsOptions = {
-  origin: ['https://expense-tracker-1580.netlify.app', 'http://localhost:5173'],
+  origin: 'https://expense-tracker-1580.netlify.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
 };
 app.use(cors(corsOptions));
-
+app.use(cors());//for local developement
 
 async function main(){
     await mongoose.connect(process.env.MONGO_URI);
